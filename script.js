d9d71f7614c63798302183c8c7c451fbbd44cab4
DeviSriPrasad9999 <68717677+DeviSriPrasad9999@users.noreply.github.com>
2021-07-12 18:34:32 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,68 @@
+document.getElementById("name").innerText = "< DEVI SRI PRASAD >";
+
+var count=0;
+function navbar(){
+    count = count +1;
+    if(count%2!=0){
+        document.querySelector("nav").style= 'background:#663399;color:black;';
+        let e=document.querySelector(".namediv").style= "margin-top:400px;transition:0.3s;"
+    }
+    else{
+        if(screen.width > 431){
+        document.querySelector("nav").style= 'background:default';
+        let e=document.querySelector(".namediv").style= "margin-top:15%;transition:0.3s;"
+        }
+        else if(screen.width <= 430){
+            document.querySelector("nav").style= 'background:default';
+            let e=document.querySelector(".namediv").style= "position:default;transition:0.3s;"
+        }
+    }   
+}
+
+function myfunc(){
+    var preloader = document.getElementById("loader");
+    setTimeout(function(){
+        preloader.style.display="none";
+    },3000);
+    console.log(navigator.appVersion);
+    
+}
+
+function home(){
+    var home = document.getElementById("namediv");
+    home.scrollIntoView({behavior:"smooth",block:"center"});
+}
+
+function about(){
+    var about = document.getElementById("about");
+    about.scrollIntoView({behavior:"smooth",block:"center"});
+}
+
+function skills(){
+    var skills = document.getElementById("skills");
+    skills.scrollIntoView({behavior:"smooth",block:"center"});
+}
+
+function contact(){
+    var contact = document.getElementById("contactme");
+    contact.scrollIntoView({behavior:"smooth",block:"center"});
+}
+
+function sendEmail(){
+    let name = document.getElementById("contactname").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("textarea").value;
+    Email.send({
+        
+        SecureToken:"d38e503a-f8ad-4f3c-8d2b-4260150f37bb",
+        // Username :"portfoliodsp@gmail.com",
+        // Password : "9603771345@Dsp",
+        To : 'devisriprasad7989@gmail.com',
+        From :email,
+        Subject : subject,
+        Body : message 
+    }).then(
+      message => alert(message)
+    );
+}
